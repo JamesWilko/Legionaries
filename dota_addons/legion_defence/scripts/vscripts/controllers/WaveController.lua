2a1a5dca06103d62a34003abea8921a4e401fe9c
James Wilkinson <jw@jameswilko.com>
2015-12-30 15:20:26 +1100

    Wave controller will count leaks as they occur over waves for each lane and adjust bounty amounts accordingly Wave controller will spawn units in each spawn zone as the enemy team instead of the spawn zones own team

--- dota_addons/legion_defence/scripts/vscripts/controllers/WaveController.lua
+++ dota_addons/legion_defence/scripts/vscripts/controllers/WaveController.lua
@@ -14,6 +14,25 @@ CWaveController.ARMOUR_TIERS = {
 	}
 }
 
+CWaveController.LEAKED_WAVE_BOUNTY_MULTIPLIERS = {
+	[1] = 1.0,
+	[2] = 1.0,
+	[3] = 1.0,
+	[4] = 0.9,
+	[5] = 0.8,
+	[6] = 0.7,
+	[7] = 0.6,
+	[8] = 0.5,
+	[9] = 0.4,
+	[10] = 0.3,
+	[11] = 0.25,
+}
+
+CWaveController.ENEMY_TEAMS = {
+	[DOTA_TEAM_GOODGUYS] = DOTA_TEAM_BADGUYS,
+	[DOTA_TEAM_BADGUYS] = DOTA_TEAM_GOODGUYS
+}
+
 function CLegionDefence:SetupWaveController()
 	self.wave_controller = CWaveController()
 	self.wave_controller:Setup()
@@ -26,6 +45,7 @@ end
 function CWaveController:Setup()
 
 	self._waves_list = LoadKeyValues("scripts/kv/legion_waves.txt")
+	self._leaks_list = {}
 
 	self._current_wave = 0
 	self._wave_in_progress = false
@@ -76,7 +96,7 @@ function CWaveController:OnThink()
 			local vPosition = RandomVectorInTrigger( ent )
 			if unit_to_spawn then
 
-				local hUnit = CreateUnitByName( unit_to_spawn, vPosition, true, nil, nil, spawn.team )
+				local hUnit = CreateUnitByName( unit_to_spawn, vPosition, true, nil, nil, self:GetEnemyTeam(spawn.team) )
 				local entTargetZone = self._map_controller:GetTargetZoneForTeam( spawn.team )
 				local vTarget = RandomVectorInTrigger( entTargetZone )
 
@@ -85,9 +105,8 @@ function CWaveController:OnThink()
 					-- TODO: Make units face spawn zone regardless of spawn position
 					hUnit:SetAngles( 0, -90, 0 )
 
-					-- TODO: Units need to be assigned to a player lane, once lane is clear (OnUnitKilled) then teleport remaining player units to the catch zone
-					self._spawned_units[spawn.team] = self._spawned_units[spawn.team] or {}
-					table.insert( self._spawned_units[spawn.team], { unit = hUnit, target = vTarget } )
+					self._spawned_units[spawn.lane] = self._spawned_units[spawn.lane] or {}
+					table.insert( self._spawned_units[spawn.lane], { unit = hUnit, target = vTarget } )
 
 				end
 
@@ -98,8 +117,8 @@ function CWaveController:OnThink()
 	end
 
 	-- Move spawned units to the target zone
-	for i, team in pairs( self._spawned_units ) do
-		for k, v in pairs( team ) do
+	for i, lane in pairs( self._spawned_units ) do
+		for k, v in pairs( lane ) do
 			if v and IsValidEntity(v.unit) then
 				v.unit:MoveToPositionAggressive( v.target )
 			else
@@ -111,6 +130,10 @@ function CWaveController:OnThink()
 	-- Think again
 	return self._think_time
 	
+end
+
+function CWaveController:GetEnemyTeam( iTeam )
+	return CWaveController.ENEMY_TEAMS[iTeam]
 end
 
 function CWaveController:GetCurrentWave()
@@ -211,12 +234,19 @@ end
 function CWaveController:OnUnitKilled( event )
 
 	local unit = EntIndexToHScript(event.entindex_killed)
-	for i, team in pairs( self._spawned_units ) do
-		for k, v in pairs( team ) do
+	for i, lane in pairs( self._spawned_units ) do
+		for k, v in pairs( lane ) do
 			if IsValidEntity(v.unit) and v.unit == unit then
 				
-				local remain = self:GetRemainingUnitsInCurrentWave()
-				if remain <= 0 then
+				-- Remaining units in this lane
+				local remainInLane = self:GetRemainingUnitsInLane(i)
+				if remainInLane <= 0 then
+					-- Teleport units to catch zone
+				end
+
+				-- Remaining units in wave
+				local remainInWave = self:GetRemainingUnitsInCurrentWave()
+				if remainInWave <= 0 then
 					self:WavePreCompleted()
 				end
 
@@ -240,8 +270,34 @@ function CWaveController:GetRemainingUnitsInCurrentWave()
 	end
 
 	-- Count units on field
-	for i, team in pairs( self._spawned_units ) do
-		for k, v in pairs( team ) do
+	for i, lane in pairs( self._spawned_units ) do
+		for k, v in pairs( lane ) do
+			if IsValidEntity(v.unit) and v.unit:IsAlive() then
+				remaining = remaining + 1
+			end
+		end
+	end
+
+	return remaining
+
+end
+
+function CWaveController:GetRemainingUnitsInLane( laneId )
+
+	if not self:IsWaveRunning() then
+		return 0
+	end
+
+	local remaining = 0
+
+	-- Count units in spawn queue
+	for k, v in pairs( self._wave_spawns_remaining ) do
+		remaining = remaining + v
+	end
+
+	-- Count units on field
+	if self._spawned_units and self._spawned_units[laneId] then
+		for k, v in pairs( self._spawned_units[laneId] ) do
 			if IsValidEntity(v.unit) and v.unit:IsAlive() then
 				remaining = remaining + 1
 			end
@@ -280,6 +336,59 @@ function CWaveController:WaveCompleted()
 
 end
 
+function CWaveController:GetUnitLane( hUnit )
+
+	for laneId, lane in pairs( self._spawned_units ) do
+		for k, v in pairs( lane ) do
+			if IsValidEntity(v.unit) and v.unit == hUnit then
+				return laneId
+			end
+		end
+	end
+
+	return nil
+
+end
+
+----------------------------------
+-- Leaked Waves
+----------------------------------
+function CWaveController:RecordWaveAsLeakedByPlayer( iPlayerId, iWave )
+
+	if iPlayerId ~= nil then
+		iWave = iWave or self:GetCurrentWave()
+		self._leaks_list[iPlayerId] = self._leaks_list[iPlayerId] or {}
+		self._leaks_list[iPlayerId][iWave] = true
+	end
+
+end
+
+function CWaveController:RecordWaveAsLeakedFromUnit( hUnit, iWave )
+
+	iWave = iWave or self:GetCurrentWave()
+	local laneId = self:GetUnitLane( hUnit )
+	local playerId = GameRules.LegionDefence:GetLaneController():GetPlayerForLane( laneId )
+	if playerId ~= nil then
+		self:RecordWaveAsLeakedByPlayer( playerId, iWave )
+	end
+
+end
+
+function CWaveController:GetNumberOfWavesLeakedByPlayer( iPlayerId )
+	if iPlayerId ~= nil and self._leaks_list[iPlayerId] then
+		return #self._leaks_list[iPlayerId]
+	end
+	return 1
+end
+
+function CWaveController:GetNumberOfWavesLeakedFromUnit( hUnit )
+	local laneId = self:GetUnitLane( hUnit )
+	local playerId = GameRules.LegionDefence:GetLaneController():GetPlayerForLane( laneId )
+	if playerId ~= nil then
+		return self:GetNumberOfWavesLeakedByPlayer( playerId )
+	end
+end
+
 ----------------------------------
 -- Unit Functions
 ----------------------------------
@@ -293,8 +402,8 @@ end
 
 function CWaveController:IsUnitAWaveUnit( hUnit )
 
-	for i, team in pairs( self._spawned_units ) do
-		for k, v in pairs( team ) do
+	for i, lane in pairs( self._spawned_units ) do
+		for k, v in pairs( lane ) do
 			if v.unit == hUnit then
 				return true
 			end
@@ -308,6 +417,24 @@ end
 function CWaveController:AttemptIncreaseArmourOnUnit( hUnit, iArmourTier )
 
 	if self:IsUnitAWaveUnit(hUnit) then
+
+		-- Add current wave to list of waves that were leaked by this player
+		self:RecordWaveAsLeakedFromUnit( hUnit )
+
+		-- Reduce this units bounty if it doesn't have an armour buff
+		local canReduceBounty = true
+		for k, v in ipairs(CWaveController.ARMOUR_TIERS) do
+			if hUnit:FindModifierByName(v.modifier) then
+				canReduceBounty = false
+				break
+			end
+		end
+		if canReduceBounty then
+			local leakedWaves = self:GetNumberOfWavesLeakedFromUnit(hUnit) or 1
+			local bountyMultiplier = CWaveController.LEAKED_WAVE_BOUNTY_MULTIPLIERS[leakedWaves]
+			hUnit:SetMinimumGoldBounty( math.floor(hUnit:GetMinimumGoldBounty() * bountyMultiplier) )
+			hUnit:SetMaximumGoldBounty( math.floor(hUnit:GetMaximumGoldBounty() * bountyMultiplier) )
+		end
 
 		-- Check unit doesn't already have this armour buff
 		local modifier = CWaveController.ARMOUR_TIERS[iArmourTier].modifier
