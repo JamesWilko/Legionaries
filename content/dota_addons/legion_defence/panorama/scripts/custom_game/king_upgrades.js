2a166f4d63c9c9983b5084fc024b41a5ec39178f
James Wilkinson <jw@jameswilko.com>
2016-01-01 12:21:38 +1100

    King upgrades will refresh correctly when the king upgrade data is updated King upgrades are slightly desaturated when not hovered over

--- content/dota_addons/legion_defence/panorama/scripts/custom_game/king_upgrades.js
+++ content/dota_addons/legion_defence/panorama/scripts/custom_game/king_upgrades.js
@@ -13,10 +13,20 @@ var m_KingUpgrades = [
 
 function OnKingUpgradeDataChanged()
 {
+	// Remove old panels
+	if(m_UpgradePanels)
+	{
+		for(var i = 0; i < m_UpgradePanels.length; ++i)
+		{
+			m_UpgradePanels[i].GetParent().RemoveAndDeleteChildren()
+		}
+		m_UpgradePanels = [];
+	}
+
+	// Add new panels
 	for(var key in m_KingUpgrades)
 	{
 		var upgrade = m_KingUpgrades[key];
-
 		var panel = $.CreatePanel( "Panel", $("#UpgradesList"), upgrade );
 		panel.BLoadLayout( "file://{resources}/layout/custom_game/king_upgrade_item.xml", true, false );
 		m_UpgradePanels[upgrade] = panel;
@@ -26,5 +36,6 @@ function OnKingUpgradeDataChanged()
 (function()
 {
 	// Net Table for King Upgrades, listen to when it updates and update our UI too
+	CustomNetTables.SubscribeNetTableListener( "KingUpgradeData", OnKingUpgradeDataChanged );
 	OnKingUpgradeDataChanged();
 })();
