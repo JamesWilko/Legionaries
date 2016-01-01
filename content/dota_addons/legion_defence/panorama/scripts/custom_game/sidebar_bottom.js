a2320b8009685a2ce75aef43f3b538adf738e3fc
James Wilkinson <jw@jameswilko.com>
2016-01-01 21:25:09 +1100

    Bottom bar upgrades won't destroy and recreate the whole UI when purchasing an upgrade

--- content/dota_addons/legion_defence/panorama/scripts/custom_game/sidebar_bottom.js
+++ content/dota_addons/legion_defence/panorama/scripts/custom_game/sidebar_bottom.js
@@ -3,30 +3,22 @@ var m_Upgrades = [];
 
 function OnUpgradesUpdated()
 {
-	// Remove old upgrades
-	if(m_Upgrades)
-	{
-		for(var i = 0; i < m_Upgrades.length; ++i)
-		{
-			if(m_Upgrades[i])
-			{
-				m_Upgrades[i].RemoveAndDeleteChildren();
-				m_Upgrades[i] = undefined;
-			}
-		}
-		m_Upgrades = [];
-	}
-
-	// Add upgrades
 	var upgradesData = CustomNetTables.GetTableValue( "Upgrades", "upgrades" );
 	if(upgradesData)
 	{
+		// Run through all upgrades
 		for(var key in upgradesData)
 		{
 			var upgrade = upgradesData[key];
-			var panel = $.CreatePanel( "Panel", $("#ChildList"), key );
+
+			// Add or update upgrade
+			var panel = $("#" + key);
+			if(!panel)
+			{
+				panel = $.CreatePanel( "Panel", $("#ChildList"), key );
+			}
 			panel.BLoadLayout( "file://{resources}/layout/custom_game/general_upgrade_item.xml", true, false );
-			m_Upgrades.push(panel.GetParent());
+			m_Upgrades[key] = panel.GetParent();
 
 			var upgradeLevel = upgrade["default"];
 			var upgradeLevelData = CustomNetTables.GetTableValue( "Upgrades", key );
