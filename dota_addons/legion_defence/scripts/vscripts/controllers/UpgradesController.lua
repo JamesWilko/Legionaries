8bae44a7e1a1ff488bc57548dd29326ee583d506
James Wilkinson <jw@jameswilko.com>
2016-01-10 16:36:22 +1100

    Upgrades can be queued up and will take a period of time to complete

--- dota_addons/legion_defence/scripts/vscripts/controllers/UpgradesController.lua
+++ dota_addons/legion_defence/scripts/vscripts/controllers/UpgradesController.lua
@@ -35,6 +35,7 @@ CUpgradeController.UPGRADES =
 				amount = 80,
 			}
 		},
+		time = 5,
 		default = 1,
 		max_level = ((CFoodController.MAXIMUM_FOOD - CFoodController.DEFAULT_FOOD) / CFoodController.FOOD_PER_LEVEL) + 1,
 		display_image = "item_tango",
@@ -51,6 +52,7 @@ CUpgradeController.UPGRADES =
 				amount = 50,
 			}
 		},
+		time = 30,
 		default = 1,
 		max_level = ((CMineController.MAXIMUM_MINERS - CMineController.DEFAULT.miners) / CMineController.UPGRADE_MINERS) + 1,
 		display_image = "item_boots",
@@ -67,6 +69,7 @@ CUpgradeController.UPGRADES =
 				amount = 80,
 			}
 		},
+		time = 10,
 		default = 1,
 		max_level = ((CMineController.MAXIMUM_MINING_SPEED - CMineController.DEFAULT.income_per_miner) / CMineController.UPGRADE_MINING_SPEED) + 1,
 		display_image = "item_mithril_hammer",
@@ -81,13 +84,20 @@ function CUpgradeController:Setup()
 
 	-- Player upgrade status
 	self._upgrades = {}
+	self._pending_upgrades = {}
 
 	-- Send upgrades info
 	CustomNetTables:SetTableValue( CUpgradeController.NET_TABLE, "upgrades", CUpgradeController.UPGRADES )
 
+	-- Think entity
+	self._think_time = 0.2
+	self._think_ent = IsValidEntity(self._think_ent) and self._think_ent or Entities:CreateByClassname("info_target")
+	self._think_ent:SetThink("OnThink", self, "UpgradeControllerThink", self._think_time)
+
 	-- Events
 	ListenToGameEvent("legion_player_assigned_lane", Dynamic_Wrap(CUpgradeController, "OnPlayerAssignedLane"), self)
 	CustomGameEventManager:RegisterListener( "legion_purchase_upgrade", Dynamic_Wrap(CUpgradeController, "HandleOnAttemptUpgradePurchase") )
+	CustomGameEventManager:RegisterListener( "legion_cancel_upgrade", Dynamic_Wrap(CUpgradeController, "HandleOnAttemptUpgradeCancel") )
 
 end
 
@@ -150,10 +160,13 @@ function CUpgradeController.HandleOnAttemptUpgradePurchase( iPlayerId_Wrong, eve
 
 		local currency_controller = GameRules.LegionDefence:GetCurrencyController()
 
-		-- Check upgrade for player isn't at max level
+		-- Verify upgrade data
 		self._upgrades[upgradeId] = self._upgrades[upgradeId] or {}
 		self._upgrades[upgradeId][iPlayerId] = self._upgrades[upgradeId][iPlayerId] or CUpgradeController.UPGRADES[upgradeId].default
+
+		-- Check upgrade for player isn't at max level
 		local playerUpgradeLevel = self._upgrades[upgradeId][iPlayerId]
+		playerUpgradeLevel = playerUpgradeLevel + self:GetPendingUpgrades( iPlayerId, upgradeId )
 		if playerUpgradeLevel >= upgrade.max_level then
 			return false, "at_max_level"
 		end
@@ -171,8 +184,50 @@ function CUpgradeController.HandleOnAttemptUpgradePurchase( iPlayerId_Wrong, eve
 			currency_controller:ModifyCurrency( cost_data.currency, iPlayerId, -cost_data.amount )
 		end
 
+		-- Queue upgrade
+		self:QueuePendingUpgrade( iPlayerId, upgradeId )
+
+	end
+
+end
+
+function CUpgradeController.HandleOnAttemptUpgradeCancel( iPlayerId_Wrong, eventArgs )
+
+	local self = GameRules.LegionDefence:GetUpgradeController()
+	local iPlayerId = eventArgs["PlayerID"]
+	local upgradeId = eventArgs["sUpgradeId"]
+	local upgrade = CUpgradeController.UPGRADES[upgradeId]
+	if upgrade and self:IsUpgradePending( iPlayerId, upgradeId ) then
+		self:DequeuePlayerUpgrade( iPlayerId, upgradeId )
+	end
+
+end
+
+function CUpgradeController:PlayerUpgradeCompleted( iPlayerId, upgradeId )
+
+	print(string.format("Player %i upgrade %s complete", iPlayerId, upgradeId))
+
+	-- Get upgrade data
+	local upgrade = CUpgradeController.UPGRADES[upgradeId]
+	if upgrade then
+
+		local currency_controller = GameRules.LegionDefence:GetCurrencyController()
+
+		-- Verify upgrade data
+		self._upgrades[upgradeId] = self._upgrades[upgradeId] or {}
+		self._upgrades[upgradeId][iPlayerId] = self._upgrades[upgradeId][iPlayerId] or CUpgradeController.UPGRADES[upgradeId].default
+
+		-- Check upgrade for player isn't at max level
+		local playerUpgradeLevel = self._upgrades[upgradeId][iPlayerId]
+		if playerUpgradeLevel >= upgrade.max_level then
+			return false, "at_max_level"
+		end
+
 		-- Increase upgrade level
 		local new_level = self._upgrades[upgradeId][iPlayerId] + 1
+		if new_level > upgrade.max_level then
+			new_level = upgrade.max_level
+		end
 		self._upgrades[upgradeId][iPlayerId] = new_level
 
 		-- Update nettable for upgrade
@@ -182,5 +237,129 @@ function CUpgradeController.HandleOnAttemptUpgradePurchase( iPlayerId_Wrong, eve
 		upgrade.func( iPlayerId, new_level, 1 )
 
 	end
+
+end
+
+function CUpgradeController:QueuePendingUpgrade( iPlayerId, upgradeId )
+	
+	local upgrade = CUpgradeController.UPGRADES[upgradeId]
+	if upgrade then
+
+		-- Verify data
+		self._pending_upgrades[upgradeId] = self._pending_upgrades[upgradeId] or {}
+		self._pending_upgrades[upgradeId][iPlayerId] = self._pending_upgrades[upgradeId][iPlayerId] or {}
+
+		-- Get upgrade data		
+		local player_data = self._pending_upgrades[upgradeId][iPlayerId]
+		local game_time = GameRules:GetDOTATime(false, false)
+		player_data.queued = player_data.queued or 0
+
+		-- Queue upgrade
+		if player_data.queued == 0 then
+			player_data.start_time = game_time
+			player_data.finish_time = game_time + upgrade.time
+		end
+		player_data.queued = player_data.queued + 1
+		player_data.is_upgrading = true
+
+		print(string.format("Player %i queued upgrade %s", iPlayerId, upgradeId))
+
+		-- Send pending upgrades to players
+		CustomNetTables:SetTableValue( CUpgradeController.NET_TABLE, "pending_" .. upgradeId, self._pending_upgrades[upgradeId] )
+
+	end
+
+end 
+
+function CUpgradeController:DequeuePlayerUpgrade( iPlayerId, upgradeId, bWasCancelled )
+
+	if bWasCancelled == nil then
+		bWasCancelled = true
+	end
+
+	local upgrade_data = CUpgradeController.UPGRADES[upgradeId]
+	local upgrade = self._pending_upgrades[upgradeId]
+	if upgrade_data and upgrade then
+		local player_data = upgrade[iPlayerId]
+		if player_data then
+
+			-- Dequeue upgrade
+			player_data.queued = player_data.queued - 1
+
+			if player_data.queued > 0 then
+
+				-- Start next queued upgrade
+				local game_time = GameRules:GetDOTATime(false, false)
+				if not bWasCancelled then
+					player_data.start_time = game_time
+					player_data.finish_time = game_time + upgrade_data.time
+				end
+				player_data.is_upgrading = true
+
+			else
+
+				-- No more upgrades
+				player_data.start_time = 0
+				player_data.finish_time = math.huge
+				player_data.is_upgrading = false
+
+			end
+
+			-- Upgrade was cancelled, refund player
+			if bWasCancelled then
+				print(string.format("Player %i cancelled upgrade %s", iPlayerId, upgradeId))
+				local currency_controller = GameRules.LegionDefence:GetCurrencyController()
+				for k, cost_data in pairs( upgrade_data.cost ) do
+					currency_controller:ModifyCurrency( cost_data.currency, iPlayerId, cost_data.amount )
+				end
+			end
+
+			-- Update nettable
+			CustomNetTables:SetTableValue( CUpgradeController.NET_TABLE, "pending_" .. upgradeId, self._pending_upgrades[upgradeId] )
+
+		end
+	end
+
+end
+
+function CUpgradeController:IsUpgradePending( iPlayerId, upgradeId )
+	local upgrade = self._pending_upgrades[upgradeId]
+	if upgrade then
+		local player_data = upgrade[iPlayerId]
+		if player_data then
+			return player_data.is_upgrading
+		end
+	end
+	return false
+end
+
+function CUpgradeController:GetPendingUpgrades( iPlayerId, upgradeId )
+	local upgrade = self._pending_upgrades[upgradeId]
+	if upgrade then
+		local player_data = upgrade[iPlayerId]
+		if player_data then
+			return player_data.queued
+		end
+	end
+	return 0
+end
+
+function CUpgradeController:OnThink()
+
+	local game_time = GameRules:GetDOTATime(false, false)
+
+	for upgrade_id, players in pairs(self._pending_upgrades) do
+		for player_id, data in pairs(players) do
+
+			-- Upgrade has completed, run upgrade
+			if data.is_upgrading and game_time >= data.finish_time then
+				self:DequeuePlayerUpgrade( player_id, upgrade_id, false )
+				self:PlayerUpgradeCompleted( player_id, upgrade_id )
+			end
+
+		end
+	end
+
+	return self._think_time
 
 end
