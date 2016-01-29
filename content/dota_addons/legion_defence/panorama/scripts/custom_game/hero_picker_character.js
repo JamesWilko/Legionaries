54be09d4ac575a379d73a533f394a399f1e68be3
James Wilkinson <jw@jameswilko.com>
2016-01-29 16:36:30 +1100

    Removed old files

--- content/dota_addons/legion_defence/panorama/scripts/custom_game/hero_picker_character.js
+++ content/dota_addons/legion_defence/panorama/scripts/custom_game/hero_picker_character.js
@@ -1,11 +1,26 @@
 
 var HEROES_NETTABLE = "HeroesList";
 var ABILITIES_KEY = "abilities";
+var MAX_ABILITIES = 6;
+
+var HeroID;
 
 function SetHero( HeroId )
 {
+	HeroID = HeroId;
+
 	$("#HeroImage").style.backgroundImage = "url( \"file://{images}/../videos/heroes/" + HeroId + ".webm\" )";
 	$("#HeroName").text = $.Localize(HeroId);
+
+	var abilities = CustomNetTables.GetTableValue( HEROES_NETTABLE, ABILITIES_KEY );
+	if(abilities)
+	{
+		for(var i = 1; i <= MAX_ABILITIES; ++i)
+		{
+			var ability = abilities[HeroID][i];
+			$("#Ability" + i).abilityname = ability;
+		}
+	}
 }
 
 function OnSelected()
@@ -15,11 +30,10 @@ function OnSelected()
 
 function ShowAbilityTooltip( AbilityId )
 {
-	var heroId = $.GetContextPanel().id;
 	var abilities = CustomNetTables.GetTableValue( HEROES_NETTABLE, ABILITIES_KEY );
 	if(abilities)
 	{
-		var ability = abilities[heroId][AbilityId];
+		var ability = abilities[HeroID][AbilityId];
 		var data = {
 			"title" : $.Localize("DOTA_Tooltip_Ability_" + ability),
 			"desc" : $.Localize("DOTA_Tooltip_Ability_" + ability + "_Description"),
@@ -33,9 +47,6 @@ function HideAbilityTooltip()
 	GameEvents.SendEventClientSide("hide_legion_tooltip", {} );	
 }
 
-(function()
-{
-
-	SetHero( $.GetContextPanel().id );
-
-})();
+Objects.Define({
+	SetHero : function( HeroId ) { SetHero(HeroId); }
+});
