7e96442ba933c425e73a9f37871254c1a262eefb
James Wilkinson <jw@jameswilko.com>
2016-02-20 23:25:19 +1100

    Fix game analytics plugin using a localized function that did not exist

--- dota_addons/legion_defence/scripts/vscripts/analytics/GameAnalytics.lua
+++ dota_addons/legion_defence/scripts/vscripts/analytics/GameAnalytics.lua
@@ -187,7 +187,7 @@ function GameAnalytics:GenerateFakeUUID()
 	-- https://gist.github.com/jrus/3197011
 	local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
 	local uuid string.gsub(template, '[xy]', function (c)
-		local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+		local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
 		return string.format('%x', v)
 	end)
 	return uuid
